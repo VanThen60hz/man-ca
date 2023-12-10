a32a8ae21f89cc528e58a2787fc843949a7bad12
VanThen60hz <nguyenvthang2409@gmail.com>
2023-12-10 14:27:32 +0700

    man-ca

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import UserTable from "./components/UserTable";
 
 function App() {
+  const userData = [
+    {
+      id: 1,
+      name: "Nguyễn Văn E",
+      dob: "09/09/2003",
+      phone: "0123456789",
+      email: "nguyenvane@gmail.com",
+      address: "Địa chỉ E",
+      role: "User",
+    },
+    {
+      id: 2,
+      name: "Nguyễn Văn A",
+      dob: "05/06/2003",
+      phone: "0123456789",
+      email: "nguyenvana@gmail.com",
+      address: "Địa chỉ A",
+      role: "User",
+    },
+    {
+      id: 3,
+      name: "Nguyễn Văn G",
+      dob: "01/01/2003",
+      phone: "0123456789",
+      email: "nguyenvang@gmail.com",
+      address: "Địa chỉ G",
+      role: "User",
+    },
+    {
+      id: 4,
+      name: "Nguyễn Văn D",
+      dob: "07/08/2003",
+      phone: "0123456789",
+      email: "nguyenvand@gmail.com",
+      address: "Địa chỉ D",
+      role: "User",
+    },
+    {
+      id: 5,
+      name: "Nguyễn Văn F",
+      dob: "11/11/1990",
+      phone: "0123456789",
+      email: "nguyenvanf@gmail.com",
+      address: "Địa chỉ F",
+      role: "User",
+    },
+    {
+      id: 6,
+      name: "Nguyễn Văn C",
+      dob: "10/03/1999",
+      phone: "0123456789",
+      email: "nguyenvanc@gmail.com",
+      address: "Địa chỉ C",
+      role: "User",
+    },
+    {
+      id: 7,
+      name: "Nguyễn Văn B",
+      dob: "04/04/2005",
+      phone: "0123456789",
+      email: "nguyenvanb@gmail.com",
+      address: "Địa chỉ B",
+      role: "User",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textTransform: "uppercase", textAlign: "center" }}>
+        Quản lý người dùng - Thông tin người dùng
+      </h1>
+      <UserTable userData={userData} />
+    </>
   );
 }
 
---
+++ src/components/UserTable.js
@@ -0,0 +1,180 @@
+import React, { useState } from "react";
+import { Button, Form, Input, Modal, Select, Table } from "antd";
+import "./UserTable.css";
+
+const UserTable = ({ userData: initialUserData }) => {
+  const [sortedInfo, setSortedInfo] = useState(null);
+  const [visibleRows, setVisibleRows] = useState(initialUserData);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRecord, setSelectedRecord] = useState(null);
+  const [form] = Form.useForm(); // Use Form hooks to access form methods
+
+  const handleChangeSelect = (value) => {
+    if (value === "all") {
+      setVisibleRows(initialUserData);
+    } else if (value === "3") {
+      setVisibleRows(initialUserData.slice(0, 3));
+    } else if (value === "5") {
+      setVisibleRows(initialUserData.slice(0, 5));
+    }
+  };
+
+  const handleSort = (columnKey) => {
+    setSortedInfo((prevSortedInfo) => {
+      const currentColumnKey = prevSortedInfo?.columnKey;
+      const currentOrder = prevSortedInfo?.order;
+
+      return {
+        columnKey,
+        order:
+          currentColumnKey === columnKey && currentOrder === "ascend"
+            ? "descend"
+            : "ascend",
+      };
+    });
+  };
+
+  const showModal = (record) => {
+    setSelectedRecord(record);
+    setIsModalOpen(true);
+    form.setFieldsValue(record); // Set form fields based on the selected record
+  };
+
+  const handleOk = () => {
+    form.validateFields().then((values) => {
+      const updatedUserData = initialUserData.map((user) =>
+        user.id === selectedRecord.id ? { ...user, ...values } : user
+      );
+
+      setVisibleRows(updatedUserData);
+
+      console.log("Updated User Data:", updatedUserData);
+
+      setIsModalOpen(false);
+    });
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const columns = [
+    {
+      title: "No",
+      dataIndex: "no",
+      render: (text, record, index) => index + 1,
+    },
+    {
+      title: "Họ Tên",
+      dataIndex: "name",
+      key: "name",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      sortOrder: sortedInfo?.columnKey === "name" && sortedInfo.order,
+    },
+    {
+      title: "Ngày sinh",
+      dataIndex: "dob",
+      key: "dob",
+      sorter: (a, b) => {
+        const dateA = new Date(a.dob);
+        const dateB = new Date(b.dob);
+        return dateA - dateB;
+      },
+      sortOrder: sortedInfo?.columnKey === "dob" && sortedInfo.order,
+    },
+    {
+      title: "SDT",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Địa chỉ",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Vai trò",
+      dataIndex: "role",
+      key: "role",
+    },
+    {
+      title: "Chỉnh sửa",
+      dataIndex: "edit",
+      key: "edit",
+      render: (_text, record) => (
+        <Button onClick={() => showModal(record)}>Chỉnh sửa</Button>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <Table
+        className="user-table"
+        dataSource={visibleRows}
+        columns={columns}
+        rowKey={(record) => record.id}
+        onChange={(_pagination, _filters, sorter) => setSortedInfo(sorter)}
+        pagination={false}
+        bordered
+      />
+      <div className="table-actions">
+        <Button onClick={() => handleSort("name")}>Sắp xếp theo tên</Button>
+        <Button onClick={() => handleSort("dob")}>Sắp xếp theo năm sinh</Button>
+
+        <Select
+          defaultValue="Tất cả"
+          onChange={handleChangeSelect}
+          options={[
+            { value: "all", label: "Tất cả" },
+            { value: "3", label: "3 hàng" },
+            { value: "5", label: "5 hàng" },
+          ]}
+        />
+
+        <Modal
+          title="Chỉnh sửa người dùng"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          okText="Lưu"
+          cancelText="Hủy"
+          style={{ textAlign: "center" }}
+        >
+          <Form
+            form={form}
+            initialValues={selectedRecord}
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 16 }}
+          >
+            <Form.Item label="Họ tên" name="name">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Ngày sinh" name="dob">
+              <Input />
+            </Form.Item>
+            <Form.Item label="SDT" name="phone">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Email" name="email">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Vai trò" name="role">
+              <Select style={{ width: "40%" }}>
+                <Select.Option value="Guest">Guest</Select.Option>
+                <Select.Option value="User">User</Select.Option>
+                <Select.Option value="Admin">Admin</Select.Option>
+              </Select>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
+    </>
+  );
+};
+
+export default UserTable;
